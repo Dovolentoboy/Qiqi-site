7e4a70a33946f62f73c4c3329ac643865b08c82b
Nikita <121382716+Dovolentoboy@users.noreply.github.com>
2023-08-26 17:24:54 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+"""Базовая модель. Абстрактный класс от которого наследуются все модели. Взял у Alenorze (никнейм человека на гитхабе)"""
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
